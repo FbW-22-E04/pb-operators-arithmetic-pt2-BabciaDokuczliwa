c1c0a6742006103617cbdf7d8441aad7e811cfd7
BabciaDokuczliwa <oblaczynska@gmail.com>
2022-06-30 16:08:45 +0200

    ex.3

---
+++ solution.js
@@ -0,0 +1,42 @@
+const snackServings = 5;
+const guests = 7;
+const together = snackServings + guests;
+console.log(together);
+
+const substracting = snackServings - guests;
+console.log(substracting);
+
+const multiply = snackServings * guests;
+console.log(multiply);
+
+const divided = snackServings / guests;
+console.log(divided);
+
+const drinks = 10;
+console.log(snackServings * guests);
+
+const task5 = snackServings * guests;
+const resultOne = task5 / drinks;
+console.log(resultOne);
+
+const monkeys = 15;
+const bananas = 9;
+console.log(monkeys / bananas);
+
+const monkeyfights = 20;
+const task7 = monkeys + bananas;
+const resultTwo = task7 * monkeyfights;
+console.log(resultTwo);
+
+const monkeyBusiness = 90;
+let boredom = 13;
+const task8 = --boredom;
+console.log(task8);
+
+const happiness = monkeys - bananas;
+const task10 = monkeyfights + happiness;
+console.log(task10);
+
+
+const lastTask = resultOne / resultTwo;
+console.log(lastTask);
